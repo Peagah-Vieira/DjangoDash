4b2509619b1f5bfd92fa1cac597a76c1cf176397
Pedro Henrique Vieira <peagahvieira2003@gmail.com>
2023-07-04 18:49:33 -0300

    feat: selenium test

---
+++ test/functional_test/users/test_users_register.py
@@ -0,0 +1,8 @@
+from .base import UsersBaseTest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+class UsersRegisterTest(UsersBaseTest):
+    def test_the_test(self):
+        ...
