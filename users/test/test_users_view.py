32915649020106c70dc741d2302aa4e2725802bb
Pedro Henrique Vieira <peagahvieira2003@gmail.com>
2023-07-03 23:53:18 -0300

    test: login and register status code

--- users/test/test_users_view.py
+++ users/test/test_users_view.py
@@ -8,6 +8,14 @@ class UsersViewsTest(TestCase):
         login_view = resolve(reverse('users:login'))
         self.assertIs(login_view.func, views.login_view)
 
+    def test_users_login_view_status_code(self):
+        response = self.client.get(reverse('users:login'))
+        self.assertEqual(response.status_code, 200)
+
     def test_users_register_view(self):
         register_view = resolve(reverse('users:register'))
         self.assertIs(register_view.func, views.register_view)
+
+    def test_users_register_view_status_code(self):
+        response = self.client.get(reverse('users:register'))
+        self.assertEqual(response.status_code, 200)
