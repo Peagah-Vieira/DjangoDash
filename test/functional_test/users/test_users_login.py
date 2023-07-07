9ecbc56e64ad5f5a8e7b155eb4565498218633cd
Pedro Henrique Vieira <peagahvieira2003@gmail.com>
2023-07-07 18:26:26 -0300

    test: logged user cant see register and login view

--- test/functional_test/users/test_users_login.py
+++ test/functional_test/users/test_users_login.py
@@ -84,3 +84,36 @@ class UsersLoginTest(UsersBaseTest):
             'Invalid username or password.',
             self.browser.find_element(By.TAG_NAME, 'body').text
         )
+
+    def test_user_logged_cant_see_login_view(self):
+        string_password = 'P@ssw0rd1'
+
+        user = User.objects.create_user(
+            username='my_user',
+            password=string_password,
+        )
+
+        # Usuário abre a página de login
+        self.browser.get(self.live_server_url + reverse('users:login'))
+
+        # Usuário vê o formulário de login
+        form = self.get_form()
+
+        username_field = self.browser.find_element(By.NAME, 'username')
+        password_field = self.browser.find_element(By.NAME, 'password')
+
+        # Usuário digita seu usuário e senha
+        username_field.send_keys(user.username)
+        password_field.send_keys(string_password)
+
+        # Usuário envia o formulário
+        form.submit()
+
+        # Usuário é redirecionado para o dashboard
+        response = self.client.get(reverse('dashboard:home'))
+        self.assertEqual(response.status_code, 302)
+
+        # Usuário tenta acessar login novamente e
+        # é redirecionado para o dashboard automaticamente
+        self.browser.get(self.live_server_url + reverse('users:login'))
+        self.assertEqual(response.status_code, 302)
---
+++ test/functional_test/users/test_users_register.py
@@ -0,0 +1,45 @@
+from .base import UsersBaseTest
+from selenium.webdriver.common.by import By
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class UsersRegisterTest(UsersBaseTest):
+    def get_form(self):
+        return self.browser.find_element(
+            By.XPATH,
+            '/html/body/div/div/div/form'
+        )
+
+    def test_user_logged_cant_see_register_view(self):
+        string_password = 'P@ssw0rd1'
+
+        user = User.objects.create_user(
+            username='my_user',
+            password=string_password,
+        )
+
+        # Usuário abre a página de register
+        self.browser.get(self.live_server_url + reverse('users:login'))
+
+        # Usuário vê o formulário de register
+        form = self.get_form()
+
+        username_field = self.browser.find_element(By.NAME, 'username')
+        password_field = self.browser.find_element(By.NAME, 'password')
+
+        # Usuário digita seu usuário e senha
+        username_field.send_keys(user.username)
+        password_field.send_keys(string_password)
+
+        # Usuário envia o formulário
+        form.submit()
+
+        # Usuário é redirecionado para o dashboard
+        response = self.client.get(reverse('dashboard:home'))
+        self.assertEqual(response.status_code, 302)
+
+        # Usuário tenta acessar register e
+        # é redirecionado para o dashboard automaticamente
+        self.browser.get(self.live_server_url + reverse('users:register'))
+        self.assertEqual(response.status_code, 302)
