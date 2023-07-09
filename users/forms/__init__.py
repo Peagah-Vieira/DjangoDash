c63b7fe2c11c6b0491d302924a90903b51a46d87
Pedro Henrique Vieira <peagahvieira2003@gmail.com>
2023-07-08 23:01:58 -0300

    feat: Custom reset form confirm inheritance

--- users/forms/__init__.py
+++ users/forms/__init__.py
@@ -2,3 +2,4 @@
 from .register_form import RegisterForm
 from .login_form import LoginForm
 from .password_reset_form import PasswordResetCustomForm
+from .password_reset_confirm_form import SetPasswordCustomForm
--- users/forms/password_reset_confirm_form.py
+++ users/forms/password_reset_confirm_form.py
@@ -1,27 +1,24 @@
 from django import forms
+from django.contrib.auth.forms import SetPasswordForm
 
 
-class LoginForm(forms.Form):
-    username = forms.CharField(
-        label='Username',
-        widget=forms.TextInput(attrs={
-            'placeholder': 'John Doe',
-            'class': 'bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'  # noqa
-        }),
-        error_messages={
-            'required': 'This field must not be empty'
-        },
-        required=True,
-    )
-
-    password = forms.CharField(
-        label='Password',
+class SetPasswordCustomForm(SetPasswordForm):
+    new_password1 = forms.CharField(
+        label="New passworda",
         widget=forms.PasswordInput(attrs={
+            'autocomplete': 'new-password',
             'placeholder': '••••••••',
             'class': 'bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'  # noqa
         }),
-        error_messages={
-            'required': 'This field must not be empty'
-        },
-        required=True,
+        strip=False,
+    )
+
+    new_password2 = forms.CharField(
+        label="New password confirmation",
+        widget=forms.PasswordInput(attrs={
+            'autocomplete': 'new-password',
+            'placeholder': '••••••••',
+            'class': 'bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'  # noqa
+        }),
+        strip=False,
     )
