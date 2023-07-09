fd21397d0fc7a2c333c97cae78b7403d0907d46a
Pedro Henrique Vieira <peagahvieira2003@gmail.com>
2023-07-09 16:57:44 -0300

    feat: Leads - category model create

--- leads/models.py
+++ leads/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
