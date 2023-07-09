fa36621886a53894d5b301e822c0cd5817c6bba2
Pedro Henrique Vieira <peagahvieira2003@gmail.com>
2023-07-09 17:09:12 -0300

    feat: Leads model create

--- leads/models.py
+++ leads/models.py
@@ -6,3 +6,14 @@ class Category(models.Model):
     description = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class Lead(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    age = models.IntegerField(default=0)
+    category = models.ForeignKey(
+        "Category", null=True, on_delete=models.SET_NULL)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
