7601e299788ce58d1ceb349edfa75f323e523cd2
Pedro Henrique Vieira <peagahvieira2003@gmail.com>
2023-07-10 20:05:00 -0300

    fix: Login required on dashboard app

--- leads/views.py
+++ leads/views.py
@@ -1,19 +1,28 @@
 from django.shortcuts import render, redirect
 from django.views import generic
-from .forms import CategoryForm
 from django.urls import reverse_lazy
 from django.contrib import messages
-from leads.models import Category
+from django.db.models import Q
 from django.shortcuts import get_object_or_404
 from django.http.response import Http404
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.contrib.messages.views import SuccessMessageMixin
 from django.core.paginator import Paginator
+from leads.models import Category
+from .forms import CategoryForm
 from utils.pagination import make_pagination_range
-from django.db.models import Q
-from django.http import JsonResponse
+
 import pandas
 
 
+@method_decorator(
+    login_required(
+        login_url='users:login',
+        redirect_field_name='next'
+    ),
+    name='dispatch'
+)
 class LeadView(generic.View):
     template_name = 'dashboard/pages/leads_overview.html'
 
@@ -33,6 +42,13 @@ class LeadView(generic.View):
         ...
 
 
+@method_decorator(
+    login_required(
+        login_url='users:login',
+        redirect_field_name='next'
+    ),
+    name='dispatch'
+)
 class CategoryView(generic.View):
     form = CategoryForm
     template_name = 'dashboard/pages/leads_category.html'
@@ -78,9 +94,25 @@ class CategoryView(generic.View):
         return render(request, self.template_name, context=context)
 
 
+@method_decorator(
+    login_required(
+        login_url='users:login',
+        redirect_field_name='next'
+    ),
+    name='dispatch'
+)
 class CategorySearchView(CategoryView):
     form = CategoryForm
     template_name = 'dashboard/pages/leads_category.html'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def setup(self, *args, **kwargs):
+        return super().setup(*args, **kwargs)
+
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
     def get(self, request):
         form = self.form()
@@ -116,12 +148,28 @@ class CategorySearchView(CategoryView):
         return render(request, self.template_name, context=context)
 
 
+@method_decorator(
+    login_required(
+        login_url='users:login',
+        redirect_field_name='next'
+    ),
+    name='dispatch'
+)
 class CategoryUpdateView(SuccessMessageMixin, generic.UpdateView):
     model = Category
     fields = ["name", "description"]
     template_name = 'dashboard/partials/category/category_table_update_modal.html'  # noqa
     success_message = 'Category updated successfully'
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def setup(self, *args, **kwargs):
+        return super().setup(*args, **kwargs)
+
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
     def get_object(self):
         _id = int(self.kwargs.get('pk'))
         category = get_object_or_404(Category, pk=_id)
@@ -131,10 +179,26 @@ class CategoryUpdateView(SuccessMessageMixin, generic.UpdateView):
         return reverse_lazy('dashboard:leads_category')
 
 
+@method_decorator(
+    login_required(
+        login_url='users:login',
+        redirect_field_name='next'
+    ),
+    name='dispatch'
+)
 class CategoryDeleteView(SuccessMessageMixin, generic.DeleteView):
     template_name = 'dashboard/partials/category/category_table_delete_modal.html'  # noqa
     success_message = 'Category deleted successfully'
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def setup(self, *args, **kwargs):
+        return super().setup(*args, **kwargs)
+
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
     def get_object(self):
         _id = int(self.kwargs.get('pk'))
         category = get_object_or_404(Category, pk=_id)
@@ -144,7 +208,23 @@ class CategoryDeleteView(SuccessMessageMixin, generic.DeleteView):
         return reverse_lazy('dashboard:leads_category')
 
 
+@method_decorator(
+    login_required(
+        login_url='users:login',
+        redirect_field_name='next'
+    ),
+    name='dispatch'
+)
 class CategoryExportView(generic.View):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def setup(self, *args, **kwargs):
+        return super().setup(*args, **kwargs)
+
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
     def get(self, request):
         categories = Category.objects.all().order_by('-id')
         data = []
