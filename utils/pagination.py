64fb20eb9c2b52e32a4bac9cdb540d32aaa54a58
Pedro Henrique Vieira <peagahvieira2003@gmail.com>
2023-07-10 01:45:38 -0300

    feat: custom pagination

---
+++ utils/pagination.py
@@ -0,0 +1,49 @@
+import math
+from django.core.paginator import Paginator
+
+
+def make_pagination_range(page_range, qty_pages, current_page):
+    middle_range = math.ceil(qty_pages / 2)
+    start_range = current_page - middle_range
+    stop_range = current_page + middle_range
+    total_pages = len(page_range)
+
+    start_range_offset = abs(start_range) if start_range < 0 else 0
+
+    if start_range < 0:
+        start_range = 0
+        stop_range += start_range_offset
+
+    if stop_range >= total_pages:
+        start_range = start_range - abs(total_pages - stop_range)
+
+    pagination = page_range[start_range:stop_range]
+    return {
+        'pagination': pagination,
+        'page_range': page_range,
+        'qty_pages': qty_pages,
+        'current_page': current_page,
+        'total_pages': total_pages,
+        'start_range': start_range,
+        'stop_range': stop_range,
+        'first_page_out_of_range': current_page > middle_range,
+        'last_page_out_of_range': stop_range < total_pages,
+    }
+
+
+def make_pagination(request, queryset, per_page, qty_pages=4):
+    try:
+        current_page = int(request.GET.get('page', 1))
+    except ValueError:
+        current_page = 1
+
+    paginator = Paginator(queryset, per_page)
+    page_obj = paginator.get_page(current_page)
+
+    pagination_range = make_pagination_range(
+        paginator.page_range,
+        qty_pages,
+        current_page
+    )
+
+    return page_obj, pagination_range
