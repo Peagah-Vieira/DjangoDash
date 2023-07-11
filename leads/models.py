e7b0493ae9af087ba9604aeaf5dae0f09b8c9aa7
Pedro Henrique Vieira <peagahvieira2003@gmail.com>
2023-07-11 05:30:42 -0300

    feat: Leads - Agent model

--- leads/models.py
+++ leads/models.py
@@ -11,6 +11,18 @@ class Category(models.Model):
         return self.name
 
 
+class Agent(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    phone_number = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.name}{self.last_name}'
+
+
 class Lead(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
@@ -18,5 +30,6 @@ class Lead(models.Model):
     age = models.IntegerField(default=0)
     category = models.ForeignKey(
         "Category", null=True, on_delete=models.SET_NULL)
+    agent = models.ForeignKey("Agent", null=True, on_delete=models.SET_NULL)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
