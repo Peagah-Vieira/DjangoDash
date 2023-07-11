2442e6ad10b67c93be8697434d478bd57fef4021
Pedro Henrique Vieira <peagahvieira2003@gmail.com>
2023-07-11 04:55:46 -0300

    refactor: Lead models update

--- leads/models.py
+++ leads/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=30)
-    description = models.CharField(max_length=255)
+    description = models.CharField(max_length=30)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -14,7 +14,7 @@ class Category(models.Model):
 class Lead(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    email = models.EmailField()
+    email = models.EmailField(max_length=30)
     age = models.IntegerField(default=0)
     category = models.ForeignKey(
         "Category", null=True, on_delete=models.SET_NULL)
