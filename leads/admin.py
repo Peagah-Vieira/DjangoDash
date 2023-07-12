49b7171468fbf15d25d674c7bb402fe7da49330d
Pedro Henrique Vieira <peagahvieira2003@gmail.com>
2023-07-11 23:11:23 -0300

    feat: Resolve Issue - Added Custom Django Admin

--- leads/admin.py
+++ leads/admin.py
@@ -3,11 +3,59 @@ from .models import Category, Agent, Lead
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    ...
+    search_fields = [
+        'id',
+        'name',
+        'description',
+        'created_at',
+        'updated_at',
+    ]
+    list_display = [
+        'id',
+        'name',
+        'description',
+        'created_at',
+        'updated_at',
+    ]
+    list_display_links = [
+        'name',
+        'description',
+    ]
+    list_filter = [
+        'created_at',
+        'updated_at',
+    ]
+    list_per_page = 10
+    ordering = ['-id']
 
 
 class AgentAdmin(admin.ModelAdmin):
-    ...
+    search_fields = [
+        'id',
+        'first_name',
+        'last_name',
+        'email',
+        'created_at',
+        'updated_at',
+    ]
+    list_display = [
+        'id',
+        'first_name',
+        'last_name',
+        'email',
+        'created_at',
+        'updated_at',
+    ]
+    list_display_links = [
+        'first_name',
+        'last_name',
+    ]
+    list_filter = [
+        'created_at',
+        'updated_at',
+    ]
+    list_per_page = 10
+    ordering = ['-id']
 
 
 class LeadAdmin(admin.ModelAdmin):
