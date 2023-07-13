2f5b093a8db88647e6385bd0fe9c477f7447061f
Peagah <peagahvieira2003@gmail.com>
2023-07-13 09:52:23 -0300

    !deploy configurations

---
+++ base_static/js/modal_update.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let form_confirm = document.querySelector('#form_confirm_update_modal')
+    let buttons = document.querySelectorAll("[data-target='#itemUpdateModal']");
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            if (button.dataset.url) {
+                form_confirm.action = button.dataset.url;
+            }
+        })
+    });
+    let confirmModal = document.getElementById("confirmUpdateButtonModal")
+    confirmModal.addEventListener('click', () => {
+        form_confirm.submit();
+    });
+});
--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -81,16 +82,25 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# Local Configuration
+# DATABASES = {
+#   'default': {
+#       'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.sqlite3'),
+#       'NAME': os.getenv('POSTGRES_DB', BASE_DIR / './db.sqlite3'),
+#       'USER': os.getenv('POSTGRES_USER', ''),
+#       'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+#       'HOST': os.getenv('POSTGRES_HOST', ''),
+#       'PORT': os.getenv('POSTGRES_PORT', ''),
+#   }
+# }
+
+# Deploy - Render Configuration
+DATABASE_URL = os.getenv('DATABASE_URL')
 DATABASES = {
-    'default': {
-        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.sqlite3'),
-        'NAME': os.getenv('POSTGRES_DB', BASE_DIR / './db.sqlite3'),
-        'USER': os.getenv('POSTGRES_USER', ''),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('POSTGRES_HOST', ''),
-        'PORT': os.getenv('POSTGRES_PORT', ''),
-    }
+    'default': dj_database_url.config(),
 }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -129,8 +139,12 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / 'base_static'
 ]
+
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
