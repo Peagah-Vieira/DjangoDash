2b9b1c7cfab377f2a9b1835e5f5f685cc7cbff66
Peagah <peagahvieira2003@gmail.com>
2023-07-13 10:06:03 -0300

    !deploy staticfiles - urlpatterns

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', include('users.urls')),
     path('dashboard/', include('dashboard.urls')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
