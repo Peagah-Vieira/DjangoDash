cc8a58ee596f6fa7e9fae612adad17618e804b10
Peagah <peagahvieira2003@gmail.com>
2023-07-17 15:20:03 -0300

    !deploy settings update

--- config/settings.py
+++ config/settings.py
@@ -83,22 +83,22 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 # Local Configuration
-DATABASES = {
-    'default': {
-        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.sqlite3'),
-        'NAME': os.getenv('POSTGRES_DB', BASE_DIR / './db.sqlite3'),
-        'USER': os.getenv('POSTGRES_USER', ''),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('POSTGRES_HOST', ''),
-        'PORT': os.getenv('POSTGRES_PORT', ''),
-    }
-}
+# DATABASES = {
+#    'default': {
+#        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.sqlite3'),
+#        'NAME': os.getenv('POSTGRES_DB', BASE_DIR / './db.sqlite3'),
+#        'USER': os.getenv('POSTGRES_USER', ''),
+#       'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+#        'HOST': os.getenv('POSTGRES_HOST', ''),
+#        'PORT': os.getenv('POSTGRES_PORT', ''),
+#    }
+# }
 
 # Deploy - Render Configuration
-# DATABASE_URL = os.getenv('DATABASE_URL')
-# DATABASES = {
-#     'default': dj_database_url.config(),
-# }
+DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASES = {
+    'default': dj_database_url.config(),
+}
 
 
 # Password validation
